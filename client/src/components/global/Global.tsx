bf882e17dd07778d9d96aacac3e77850c8b15067
Mat <matze.krieger@googlemail.com>
2025-08-29 22:37:20 +0200

    (client) started adding Global-component

---
+++ client/src/components/global/Global.tsx
@@ -0,0 +1,12 @@
+       //! Global-component
+    // here is my hero component that is going to hold our header, footer and other nav-elements
+    
+import Link from 'next/link';
+import { StrapiImage } from '../StrapiImage';
+import type { HeroProps } from '@/types';
+
+import Image from "next/image";
+
+
+// this will be our global-elements that are modelled in the api-endboints
+export function GlobalElements({}){}
