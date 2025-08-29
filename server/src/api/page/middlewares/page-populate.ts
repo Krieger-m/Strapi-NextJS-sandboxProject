84a63e95034b5a9def5c8f4c9da5d215a07c9c3f
Mat <matze.krieger@googlemail.com>
2025-08-29 22:15:28 +0200

    (server) restored default page routes and middleware

--- server/src/api/page/middlewares/page-populate.ts
+++ server/src/api/page/middlewares/page-populate.ts
@@ -5,75 +5,15 @@
 import type { Core } from '@strapi/strapi';
 
 
-  //! ctxQuery function 
-  // this prints out a representation of the population object
-const printCtxQuery = (ctx)=>{
-  return console.dir(
-    ctx.query,
-    {depth: null}
-  );
-};
-
   //! custom population-object
-  // predefined endpoint parameters (json)
+  // default populate-all
 const populate = '*';
-// const populate = {
-//   blocks:{ 
-//     on:{ 
-//       "blocks.hero":{ 
-//         populate:{ 
-//           links: true, 
-//           image:{ fields: ["alternativeText", "url"] } 
-//         } 
-//       },
-//       "blocks.section-heading": true,
-//       "blocks.card-grid":{ 
-//         populate:{
-//           cards: true,
-//         }
-//       },
-//       "blocks.content-with-image": {
-//         populate:{
-//           link: true,
-//           image:{ fields: [ "alternativeText", "url"]},
-//         }
-//       },
-//       "blocks.markdown": true,
-//       "blocks.person-card": {
-//         populate:{
-//           image:{ fields: [ "alternativeText", "url"]}, 
-//         }
-//       },
-//       "blocks.faqs":{
-//         populate:{
-//           faq: true,
-//         }
-//       },
-//       "blocks.newsletter": true,
-//     } 
-//   }
-// }
 
 
 export default (config, { strapi }: { strapi: Core.Strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
-      //! my modified code 
-
-    console.log('\n'+
-      '\t-> custom page-population middleware function executing ...\n'+
-      '\t***********************************************************'
-    );
-
-      // overwrites population request with default-parameters
-    ctx.query.populate = populate;
     
-      // object data is being printed here
-    console.log('\nctx.query: \n');
-    printCtxQuery(ctx);
-    console.log('\n')
-
-      //!
 
     strapi.log.info('In page-populate middleware.');
 
